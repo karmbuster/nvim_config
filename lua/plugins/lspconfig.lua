2e9b661a987d0cac041cd2aada9406be5838bfca
Kevin Armbruster <karmbruster22@gmail.com>
2024-03-24 15:00:34 -0500

    adding LSP

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,13 @@
+return{
+  "williamboman/mason.nvim",
+  require("mason").setup({
+    ui = {
+      icons = {
+        package_installed = "✓",
+        package_pending = "➜",
+        package_uninstalled = "✗"
+      }
+    }
+  })
+}
+
