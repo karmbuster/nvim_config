5303077fbcdd277bae44172292ec176adfcca0ea
Kevin Armbruster <karmbruster22@gmail.com>
2024-03-29 12:37:36 -0500

    fix formatting and add autocompletions

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,22 +1,22 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-		configs.setup({
-			ensure_installed = {
-				"lua",
-				"html",
-				"css",
-				"json",
-				"javascript",
-				"typescript",
-				"python",
-			},
-			sync_install = false,
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      ensure_installed = {
+        "lua",
+        "html",
+        "css",
+        "json",
+        "javascript",
+        "typescript",
+        "python",
+      },
+      sync_install = false,
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
 }
